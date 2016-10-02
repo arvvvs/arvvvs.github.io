b8bbf8dbff7147eaaf1fdd4ef27a2f050e539fb2
Akshay Verma <arvvvs@gmail.com>
2016-10-02 16:39:43 -0400

    finishing up some stuff

--- dist/controllers/maincontroller.js
+++ dist/controllers/maincontroller.js
@@ -15,5 +15,6 @@ var personalWebsite;
             typeSpeed: 45,
         });
     });
+    console.log("Hello there! If you're looking at this it means that you're looking at my websites code.  I built this website using Angular, Angular Material, TypeScript, SASS and a few other tools.  I'm always making improvements to it as I learn new things, so it's never really finished. \nI'm currently looking for a full time developer position so if you have any openings please let me know! My email is averma2@binghamton.edu");
 })(personalWebsite || (personalWebsite = {}));
 //# sourceMappingURL=maincontroller.js.map
