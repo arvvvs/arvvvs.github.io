796ee23d9fdc3f52073cb7241d4df4380dcb5f35
Akshay Verma <arvvvs@gmail.com>
2016-10-03 18:01:54 -0400

    retest

--- dist/boot.js
+++ dist/boot.js
@@ -3,6 +3,10 @@ var personalWebsite;
 (function (personalWebsite) {
     angular
         .module('personalWebsite', ['ngMaterial', 'ngAnimate'])
-        .controller('mainController', personalWebsite.MainController);
+        .controller('mainController', personalWebsite.MainController)
+        .config(['$compileProvider', function ($compileProvider) {
+            $compileProvider.debugInfoEnabled(true);
+            console.log('its true');
+        }]);
 })(personalWebsite || (personalWebsite = {}));
 //# sourceMappingURL=boot.js.map
--- src/boot.ts
+++ src/boot.ts
@@ -5,6 +5,10 @@ module personalWebsite{
 	angular
 		.module('personalWebsite', ['ngMaterial', 'ngAnimate'])
 		.controller('mainController', MainController)
+		.config(['$compileProvider', function ($compileProvider) {
+			$compileProvider.debugInfoEnabled(true);
+			console.log('its true');
+		}]);
 }	
 
 
